d5e730d4c21daed295d7615bd37d584aabe7a665
Eduard Khiaev <eddietheone1984@gmail.com>
2019-10-17 15:29:39 +0300

    separate App URLs Mapping URLs to Views

--- phonebook_app/views.py
+++ phonebook_app/views.py
@@ -3,5 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def home(request):
-    return HttpResponse('<h1>Hello world</h1>')
+def phone_list(request):
+    return HttpResponse('<h1>phone_list</h1>')
+
+
+def phone_detail(request):
+    return HttpResponse('<h1>phone_detail</h1>')
+
+
+def phone_create(request):
+    return HttpResponse('<h1>phone_create</h1>')
+
+
+def phone_update(request):
+    return HttpResponse('<h1>phone_update</h1>')
+
+
+def phone_delete(request):
+    return HttpResponse('<h1>phone_delete</h1>')
