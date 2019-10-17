68152ca803c91e8950680622de2f775431dae13f
Eduard Khiaev <eddietheone1984@gmail.com>
2019-10-17 15:14:10 +0300

    basic Views and URL

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from phonebook_app import views as phonebook_app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', phonebook_app_views.home, name='home'),
 ]
